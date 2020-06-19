9c43bed658990ee8ab538dad16d3b7d530852ddf
valoeghese <valoeghesemods@gmail.com>
2020-06-19 22:09:41 +1200

    yeet

---
+++ src/main/java/tk/valoeghese/teleport/Mod.java
@@ -0,0 +1,10 @@
+package tk.valoeghese.teleport;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Setting up Illustrious Gateways");
+	}
+}
